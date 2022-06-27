214bc95898829ac07e1a54a6304b6576e8c8bd19
LucasHBG <lukahbg@gmail.com>
2022-06-27 17:22:54 -0300

    Accepted solution w/ sliding window tec

--- leetcode/MaximumPointsYouCanObtainFromCards/src/App.java
+++ leetcode/MaximumPointsYouCanObtainFromCards/src/App.java
@@ -2,16 +2,24 @@ public class App {
 
     public static int maxScore(int[] cardPoints, int k) {
         int arr_size = cardPoints.length;
+
+        int l = 0;
+        int r = arr_size - k;
         
-        int sum = Math.max(cardPoints[0], cardPoints[arr_size - 1]);
+        int window_sum = 0;
         
-        for(int i = 1; i < k; i++){
-            sum += Math.max(cardPoints[i], cardPoints[arr_size - 1 - i]);
-            System.out.println("Leftmost: " + cardPoints[i]);
-            System.out.println("Rightmost: " + cardPoints[arr_size - 1 - i]);
+        for(int i = r; i < arr_size; i++) window_sum += cardPoints[i];
+        
+        int max_sum = window_sum;
+
+        while(r < arr_size){
+            window_sum += cardPoints[l] - cardPoints[r];
+            max_sum = Math.max(window_sum, max_sum);
+            l += 1;
+            r += 1;
         }
         
-        return sum;
+        return max_sum;
     }
 
     public static void main(String[] args) throws Exception {
